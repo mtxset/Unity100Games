caf0449123ca9609221a19fff5f44d1d876f11f5
Romanas <gensatus@gmail.com>
2021-01-15 23:00:02 +0200

    for builds disabling editor scripts;

--- Assets/Standard Assets/Minigames/21-30/Minigolf/EditorController.cs
+++ Assets/Standard Assets/Minigames/21-30/Minigolf/EditorController.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 
@@ -22,3 +23,4 @@ namespace Minigames.Minigolf {
          }
     }
 }
+#endif
--- Assets/Standard Assets/Minigames/21-30/Rex/RexControllerEditor.cs
+++ Assets/Standard Assets/Minigames/21-30/Rex/RexControllerEditor.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 using Minigames.Rex;
@@ -12,3 +13,4 @@ public class RexControllerEditor : Editor {
             myScript.Jump();
     }
 }
+#endif
